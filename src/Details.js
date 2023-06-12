326faa7a952603272a5fac0e85767e166740a1ad
thanush <thanush38@outlook.com>
2023-06-12 17:25:06 -0400

    doesnt work

--- src/Details.js
+++ src/Details.js
@@ -5,19 +5,20 @@ function Details(props){
     const printtypes = () => {
         console.log(props.pokemon.types)
     }
+    
     // const tt=[]
     // const types = props.pokemon.types
     
     // tt.push(types[0].type.name)
     
     // console.log(tt)
-    console.log(props.pokemon)
+    // console.log(props.pokemon)
 
     return(
         <div>
             <p>name: {props.pokemon.name}</p>
             {/* {types()} */}
-            {/* <p>type: {tt[0]}</p> */}
+            {/* <p>type: {props.pokemon.types[0].type.name}</p> */}
             <p>height: {props.pokemon.height}</p>
             <p>weight: {props.pokemon.weight}</p>
 
--- src/Input.js
+++ src/Input.js
@@ -4,29 +4,33 @@ import Details from "./Details.js";
 import axios from "axios";
 function Input(){
     const [pokemon,setPokemon] = useState("");
+    const [pokemonData,setPokemonData] = useState([]); // [{}
     const [show,setShow] = useState(false);
     function handleChange(event){
         setPokemon(event.target.value);
     }
-    function findPokemon(){
-        setShow(true);
+   
+    const findPokemon= async () => {
+        
         const url = "https://pokeapi.co/api/v2/pokemon/"+pokemon;
         try{
             fetch(url)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                setPokemon(data);
+                setPokemonData(data);
             })
         } catch(error){
             console.log(error);
         }
+        setShow(true);
     }
     return(
         <div>
             <input type="text" placeholder="Search Pokemon" onChange={handleChange}/>
             <button onClick={findPokemon}>Search</button>
-            {show ? <Details pokemon={pokemon}/> : null}
+            {show ? <Details pokemon={pokemonData}/> : null}
+            {show ? <p>type: {pokemonData.types[0].type.name}</p>: null}
         </div>
     )
 }
